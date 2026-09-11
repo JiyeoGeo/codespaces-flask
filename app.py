68464525665f5c39f011a6bcbf0f6141ab27f90b
JiyeoGeo <capir.2025116isinf0003@aluno.ifpi.edu.br>
2026-09-11 10:49:28 +0000

    adiciona novas rotas e templates para a aplicação Flask, incluindo páginas de cálculo e sobre

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+
+@app.route('/calculadora')
+def calculadora():
+    return render_template('calculadora.html')
+
+
+@app.route('/resultado')
+def resultado():
+    resultado = request.args.get('resultado')
+    return render_template('resultado.html', resultado=resultado)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
